263374c8d7141a1ab29b6e9d986f3df6bd61bec3
Jackson1903 <randallcerdasjckson@gmail.com>
2015-07-14 20:53:56 -0600

    Initial Commit

---
+++ YourTeamChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YourTeamChallenge
+//
+//  Created by Randall Cerdas Jackson on 7/14/15.
+//  Copyright (c) 2015 Randall Cerdas Jackson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
